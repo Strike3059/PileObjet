b796387ac3a2b95162b28b75edcf7dec28675aa1
jonathan.simon <jonathan.simon2@outlook.fr>
2022-01-24 11:11:45 +0100

    Finalisation PileObjet

---
+++ PileObjet/Pile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MesOutils
+{
+    /// <summary>
+    /// Pile contenant pouvant contenir des entiers.
+    /// On ajoute après le dernier élément ajouté
+    /// On retire toujours le dernier élément ajouté
+    /// </summary>
+    class Pile
+    {
+        /// <summary>
+        /// Nombre maximum d'élements de la Pile
+        /// </summary>
+        private int nbMaxElt;
+
+        /// <summary>
+        ///  Liste contenant les éléments de la Pile
+        /// </summary>
+        private List<int> elements;
+
+        public Pile(int nbMaxElt)
+        {
+            this.nbMaxElt = nbMaxElt;
+            this.elements = new List<int>();
+        }
+
+        public bool PileVide()
+        {
+            return this.elements.Count <= 0;
+        }
+
+        public bool PilePleine()
+        {
+            return this.elements.Count >= this.nbMaxElt;
+        }
+
+        public void Empiler(int val)
+        {
+            if (!PilePleine())
+            {
+                this.elements.Add(val);
+            }
+            else
+            {
+                throw new Exception("Pile pleine, impossible d'empiler un élément");
+            }
+        }
+        public int Depiler()
+        {
+
+            if (PileVide())
+            {
+                throw new Exception("Impossible de dépiler, pile vide");
+            }
+            int val = this.elements[this.elements.Count - 1];
+            this.elements.RemoveAt(this.elements.Count - 1);
+            return val;
+        }
+    }
+}
--- PileObjet/Program.cs
+++ PileObjet/Program.cs
@@ -4,12 +4,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PileObjet
+namespace MesOutils
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //TestPileVidePleine(5);
+                //TestPileVidePleine(0);
+                //TestEmpiler(5);
+                //TestEmpiler(2);
+
+                TesteEmpilerDepiler(5);
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Fin du programme");
+            Console.ReadKey();
+        }
+
+        static void TestPileVidePleine(int nbElements)
+        {
+            Pile unePile = new Pile(nbElements);
+            if (unePile.PileVide())
+            {
+                Console.WriteLine("La pile est vide");
+            }
+            else
+            {
+                Console.WriteLine("La pile n'est pas vide");
+            }
+
+            if (unePile.PilePleine())
+            {
+                Console.WriteLine("La pile est pleine");
+            }
+            else
+            {
+                Console.WriteLine("La pile n'est pas pleine");
+            }
+
+        }
+
+        static void TestEmpiler(int nbElements)
+        {
+            Pile unePile = new Pile(nbElements);
+            unePile.Empiler(2);
+            unePile.Empiler(14);
+            unePile.Empiler(6);
+        }
+
+        static void TesteEmpilerDepiler(int nbElements)
+        {
+            try
+            {
+                Pile unePile = new Pile(nbElements);
+                unePile.Empiler(2);
+                unePile.Empiler(22);
+                unePile.Empiler(14);
+                unePile.Empiler(12);
+                unePile.Empiler(5);
+                int valeurDepilee = (int)unePile.Depiler();
+                Console.WriteLine("valeur dépilée : " + valeurDepilee);
+                valeurDepilee = (int)unePile.Depiler();
+                valeurDepilee = (int)unePile.Depiler();
+                valeurDepilee = (int)unePile.Depiler();
+                valeurDepilee = (int)unePile.Depiler();
+                //valeurDepilee = (int)unePile.Depiler();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
